bab4a9623f9d661ab84188b75c93459fc9074fd6
jwihardi <84292598+jwihardi@users.noreply.github.com>
2025-01-07 14:58:35 -0800

    Python

---
+++ calculator.py
@@ -0,0 +1,27 @@
+def calculator() -> None:   # hint at no return
+    print("***************************")
+    print("SIMPLE CALCULATOR IN PYTHON")
+    print("ENTER INPUT WITHOUT SPACES")
+    print("***************************")
+
+    while True:
+        option = input("Enter an operation (+, -, *, /) | 'q' to quit: ")
+        if option == 'q' or option == 'Q':
+            break
+        num_1 = (float)(input("Enter number 1 (double): "))
+        num_2 = (float)(input("Enter number 2 (double): "))
+        total = 0 
+
+        match option:
+            case '+':
+               total = num_1 + num_2
+            case '-':
+                total = num_1 - num_2
+            case '*':
+                total = num_1 * num_2
+            case '/':
+                total = num_1 / num_2
+        
+        print("The result is", total)
+
+calculator()    # call function since python doesn't need an initial function
