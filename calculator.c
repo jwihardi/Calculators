14dd8a8768b04bae9e36de1a683462d214c41200
jwihardi <84292598+jwihardi@users.noreply.github.com>
2025-01-07 15:12:47 -0800

    C

--- calculator.c
+++ calculator.c
@@ -1,4 +1,4 @@
-#include <stdio.h>  /* needed for printf and scanf*/
+#include <stdio.h> /* needed for printf and scanf*/
 
 int main(){
   printf("***************************\n");
@@ -6,17 +6,17 @@ int main(){
   printf("ENTER INPUTS WITHOUT SPACES\n");
   printf("***************************\n\n\n");
 
-  while(1){   /* no booleans (1 == true) */
+  while(1){ /* no booleans (1 == true) */
     /* variables must be declared at a block's head */
     char option;
     double num_1, num_2, total = 0;
     printf("Enter an operation (+, -, *, /) | 'q' to quit: ");
-    scanf(" %c", &option);  /* scan in the char input (%c) into the address of option */
+    scanf(" %c", &option); /* scan in the char input (%c) into the address of option */
     if(option == 'q' || option == 'Q') break;
     printf("Enter number 1 (double): ");
-    scanf(" %lf", &num_1);   /* scan in the double input (%lf) into the address of num_1 */
+    scanf(" %lf", &num_1); /* scan in the double input (%lf) into the address of num_1 */
     printf("Enter number 2 (double): ");
-    scanf(" %lf", &num_2);   /* scan in the double input (%lf) into the address of num_2 */
+    scanf(" %lf", &num_2); /* scan in the double input (%lf) into the address of num_2 */
 
     switch(option){
       case '+': total = num_1 + num_2;
