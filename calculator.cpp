ce1349ccbc2970bfe07b251286441ab0ae6c2e0c
jwihardi <84292598+jwihardi@users.noreply.github.com>
2025-01-07 14:55:46 -0800

    C++

---
+++ calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>   // needed for std::cout and std::cin
+
+using std::cout, std::cin;  // better than doing "using namespace std;"
+int main(){
+  cout << "**************************\n";
+  cout << "A SIMPLE CALCULATOR IN C++\n";
+  cout << "ENTER INPUT WITHOUT SPACES\n";
+  cout << "**************************\n\n\n";
+
+  while(true){
+    cout << "Enter an operation (+, -, *, /) | 'q' to quit: ";
+    char option;
+    cin >> option;  // get char input
+    if(option == 'q' || option == 'Q') break;
+    double num1, num2, total;
+    cout << "Enter number 1 (double): ";
+    cin >> num1; 
+    cout << "Enter number 2 (double): ";
+    cin >> num2;
+
+    switch(option){
+      case '+': total = num1 + num2;
+      break;
+      case '-': total = num1 - num2;
+      break;
+      case '*': total = num1 * num2;
+      break;
+      case '/': total = num1 / num2;
+      break;
+    }
+    cout << "The result is " << total << "\n";
+  }
+  return 0;
+}
