7c88045c95f31752864cf8d1c6ef033d4e804713
jwihardi <84292598+jwihardi@users.noreply.github.com>
2025-01-07 15:19:27 -0800

    Bash script

---
+++ calculator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+function calculator(){
+  echo "***************************"
+  echo "A SIMPLE CALCULATOR IN BASH"
+  echo "ENTER INPUTS WITHOUT SPACES"
+  echo -e "***************************\n\n" # -e to recognize '\n'
+
+  while true;
+  do
+    read -p "Enter an operation (+, -, *, /) | 'q' to quit: " option # -p to print user for input
+    if [ "$option" == "q" ] || [ "$option" == "Q" ]; then 
+      break 
+    fi
+    # Bash only has integer math, I would have to use 'bc' but I don't want to 
+    read -p "Enter number 1 (integer): " num1
+    read -p "Enter number 2 (integer): " num2
+    total=0
+ 
+    case "$option" in
+      "+")
+        total="$((num1+num2))"
+        ;;
+      "-")
+        total="$((num1-num2))"
+        ;;
+      "*")
+        total="$((num1*num2))"
+        ;;
+      "/")
+        total="$((num1/num2))"
+        ;;
+    esac
+    echo "The result is: $total"
+  done
+  }
+
+calculator
